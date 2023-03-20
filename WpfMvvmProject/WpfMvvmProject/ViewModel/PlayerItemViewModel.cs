0e0145449965da6fa82c2032f569fb4d3f1b1f0e
tarkk <tarkank@live.com>
2023-03-20 16:52:14 -0400

    Added DelegateCommand

---
+++ WpfMvvmProject/WpfMvvmProject/ViewModel/PlayerItemViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMvvmProject.Model;
+
+namespace WpfMvvmProject.ViewModel
+{
+    public class PlayerItemViewModel : ViewModelBase
+    {
+        private readonly Player _model;
+
+        public PlayerItemViewModel(Player model)
+        {
+            _model = model;
+        }
+
+        public int Id => _model.Id;
+
+        public string? FirstName
+        {
+            get => _model.FirstName;
+            set 
+            { 
+                _model.FirstName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        
+        public string? LastName
+        {
+            get => _model.LastName;
+            set 
+            { 
+                _model.LastName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string? Position
+        {
+            get => _model.Position;
+            set 
+            {
+                _model.Position = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool IsRetired
+        { 
+            get => _model.IsRetired;
+            set
+            {
+                _model.IsRetired = value;
+                RaisePropertyChanged();
+            }
+        }
+
+    }
+}
